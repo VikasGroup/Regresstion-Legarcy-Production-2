d68efc7e461d3c5784d7c30dd731dee479e0a240
vikasgroup <qaauto@vikas.lk>
2017-04-03 13:42:06 +0530

    First Commit

---
+++ src/test/java/com/test/leg/reg/vo2/vo2/DefinitionTestSuite.java
@@ -0,0 +1,9 @@
+package com.test.leg.reg.vo2.vo2;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features="src/test/resources/features/consult_dictionary/LookupADefinition.feature")
+public class DefinitionTestSuite {}
